b819d8e9a5c58a6d3b6da57856403b4a781dad9c
Yun  Ru <ruyun@p14.ecf.utoronto.ca>
2023-09-19 18:28:22 -0400

    Replay and modify example in Chapter 3 - activity 3 add nav bar,title,timestamp

--- hello.py
+++ hello.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 # index page
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    # return '<h1>Hello World!</h1>'
+    return render_template('index.html',current_time=datetime.utcnow())
 
 @app.route('/user/<name>/')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    # return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html',name =name ,current_time=datetime.utcnow())
